e5a0eb96d00efd29c99f48d7d7ff6519df3cb5b3
loiodicj1 <loiodicj1@newpaltz.edu>
2022-12-02 15:12:32 -0500

    server package and running

--- server/index.js
+++ server/index.js
@@ -1,19 +1,39 @@
-const http = require('http');
+const express = require('express')
+const app = express()
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Happy New Year');
+app.use('/', express.static('./client/dist'));
+app.use(express.json());
+app
+.get('/', (req, res) => {
+    res.status(200).send('Happy Sweet New Year');
+})
+.get('/error', (req, res) => {
+    sss.PORT();
+})
 
-});
+app.get('*', (req, res) => {
+  res.sendFile('index.html', {root: './client/dist'});
+})
 
-server.listen(port, hostname, () => {
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status( err.httpCode ?? 500).send({
+        message: err.message ?? 'Something went wrong',
+        status: err.httpCode ?? 500
+    });
+
+})
+
+app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-    console.log('Server still running at http://' + hostname + ':' + port + '/');
 });
-
-const fatArrow = x => x * 25;
