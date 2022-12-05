c67cac20dafd4c729f2441e5c673f5ef49b7e409
loiodicj1 <loiodicj1@newpaltz.edu>
2022-12-04 19:36:38 -0500

    render debugging try 3

--- server/models/mongo.js
+++ server/models/mongo.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.MONGO_URI ?? "";
-console.log("mongo.js mongo: " + uri)
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }
 
