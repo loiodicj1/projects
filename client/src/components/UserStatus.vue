5667ea7efc43eac3d003b2a2345e72f397f1e0b0
loiodicj1 <loiodicj1@newpaltz.edu>
2022-12-05 16:51:12 -0500

    Alphabetized users login dropdown, loading effect

--- client/src/components/UserStatus.vue
+++ client/src/components/UserStatus.vue
@@ -19,30 +19,39 @@
 </script>
 
 <template>
-    <div class="buttons" v-if="session.user == null">
-
-			<div class="dropdown is-active is-right">
-				<div class="dropdown-trigger">
-					<button v-if="session.user == null" class="button" aria-haspopup="true" aria-controls="dropdown-menu3" @click="updateUsersList(); userListDropDownActive = !userListDropDownActive">
-						<span>Log in</span>
-					</button>
-				</div>
-				<div v-if="session.user == null" class="dropdown-menu" id="dropdown-menu3" role="menu" v-show="userListDropDownActive">
-					<div class="dropdown-content">
-						<div v-for="user in users" :key="user.name"> <!--programmatically create list of users-->
-							<a href="#" class="dropdown-item" @click="userListDropDownActive = !userListDropDownActive; login(user.name)">
-								{{user.name}}
-							</a>
+	<div v-if="(session.loading == 0)">
+		<div class="buttons" v-if="session.user == null">
+				<div class="dropdown is-active is-right">
+					<div class="dropdown-trigger">
+						<button v-if="session.user == null" class="button" aria-haspopup="true" aria-controls="dropdown-menu3" @click="updateUsersList(); userListDropDownActive = !userListDropDownActive">
+							<span>Log in</span>
+						</button>
+					</div>
+					<div v-if="session.user == null" class="dropdown-menu" id="dropdown-menu3" role="menu" v-show="userListDropDownActive">
+						<div class="dropdown-content">
+							<div v-for="user in users.sort( (u0, u1) => {
+								return u0.name.localeCompare(u1.name)
+							})" :key="user.name"> <!--programmatically create list of users-->
+								<a href="#" class="dropdown-item" @click="userListDropDownActive = !userListDropDownActive; login(user.name)">
+									{{user.name}}
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+		<div class="buttons" v-else>
+				<a class="button is-danger" @click="logout()">
+			Log out
+		</a>
 		</div>
-    <div class="buttons" v-else>
-			<a class="button is-danger" @click="logout()">
-        Log out
-      </a>
-    </div>
+	</div>
+
+	<div v-if="(session.loading > 0)">
+		<button class="button is-warning" @click="logout()">               
+			<i class="fa-solid fa-loader">Loading</i>
+		</button>
+	</div>
 </template>
 
 
